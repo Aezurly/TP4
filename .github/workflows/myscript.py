1b86ab4caf862c8cd6564e5263880672268847ec
Aezurly <66571923+Aezurly@users.noreply.github.com>
2025-03-23 20:27:52 -0400

    Partie C

---
+++ .github/workflows/myscript.py
@@ -0,0 +1,15 @@
+import os
+
+BAD_HASH = "c1a4be04b972b6c17db242fc37752ad517c29402"
+GOOD_HASH = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
+
+def main():
+    os.system(f"git bisect start {BAD_HASH} {GOOD_HASH}")
+    
+    exit_code = os.system("git bisect run python manage.py test")
+    os.system("git bisect reset")
+    
+    exit(exit_code)
+
+if __name__ == "__main__":
+    main()
